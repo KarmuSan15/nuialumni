518c8024c77b00464a4af1879706b7f934b1266f
Toruh D.L <toruhlahera@gmail.com>
2025-01-20 21:22:58 +0000

    EjARToruh.jsx

--- src/components/toruhAR/EjARToruh.jsx
+++ src/components/toruhAR/EjARToruh.jsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment,OrbitControls } from '@react-three/drei'
-import Barry3D from './ToruhModel2';
+import ToruhModel2 from './ToruhModel2';
 
 export default function EjARToruh() {
   return (
     <div className="App">
       <Canvas>
         <Suspense fallback={null}>
-          <Barry3D />
+          <ToruhModel2 />
           <Environment preset="sunset" background/>
           <OrbitControls />
         </Suspense>
